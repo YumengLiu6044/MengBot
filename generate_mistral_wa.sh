a8e53ce3ddf330ef6e06da1b4a1efabdc9770a14
Yumeng Liu <lym20041026@gmail.com>
2024-11-13 16:14:01 -0800

    init

--- generate_mistral_wa.sh
+++ generate_mistral_wa.sh
@@ -1,1 +1,1 @@
-base_model_id="alpindale/Mistral-7B-v0.2-hf" lora_weights="../data" CUDA_VISIBLE_DEVICES=0 uvicorn generate_mistral:app --port 9092 --host 0.0.0.0 --reload
+base_model_id="alpindale/Mistral-7B-v0.2-hf" lora_weights="../model" CUDA_VISIBLE_DEVICES=0 uvicorn generate_mistral:app --port 9092 --host 0.0.0.0 --reload
