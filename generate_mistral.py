379fdc0273f5191a13a3e682b0907bc5a6514a9d
Yumeng Liu <lym20041026@gmail.com>
2024-11-13 17:20:38 -0800

    8 bit

--- generate_mistral.py
+++ generate_mistral.py
@@ -17,16 +17,14 @@ base_model_id = os.getenv("base_model_id")
 lora_weights = os.getenv("lora_weights")
 
 
-# bnb_config = BitsAndBytesConfig(
-#     load_in_4bit=True,
-#     bnb_4bit_use_double_quant=True,
-#     bnb_4bit_quant_type="nf4",
-#     bnb_4bit_compute_dtype=torch.bfloat16
-# )
+bnb_config = BitsAndBytesConfig(
+    load_in_8bit=True
+)
 
 
 base_model = AutoModelForCausalLM.from_pretrained(
     pretrained_model_name_or_path=base_model_id,
+    quantization_config=bnb_config,  
     trust_remote_code=True,
     token=True
 )
